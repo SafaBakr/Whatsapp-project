01cb6ad90f75eea7f0ef1143fa8fb108cd806a0f
SafaAhmedBakr <safabakr6.2@gmail.com>
2022-12-01 10:54:19 -0800

    Create layout_chat_page.dart

---
+++ layout_chat_page.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_project/colors_app.dart';
+import 'package:whatsapp_project/WidgetsApp/contacts.dart';
+
+class PageLayout extends StatelessWidget {
+  const PageLayout({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: appBarColor,
+          centerTitle: false,
+          title: const Text(
+            'WhatsApp',
+            style: TextStyle(
+              color: Colors.grey,
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+          ),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.search, color: Colors.grey),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: const Icon(Icons.more_vert, color: Colors.grey),
+              onPressed: () {},
+            ),
+          ],
+          bottom: const TabBar(
+            indicatorColor: tabColor,
+            indicatorWeight: 4,
+            labelColor: tabColor,
+            unselectedLabelColor: Colors.grey,
+            labelStyle: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+            tabs: [
+              Tab(
+                text: 'CHATS',
+              ),
+              Tab(
+                text: 'STATUS',
+              ),
+              Tab(
+                text: 'CALLS',
+              ),
+            ],
+          ),
+        ),
+        body: const Contacts(),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: tabColor,
+          child: const Icon(
+            Icons.comment,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
